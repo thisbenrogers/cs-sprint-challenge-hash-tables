18937ee272eaf7453ae2d0df3dc047268845f1a8
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-09-11 22:52:53 -0500

    Attempts indeces_of_weight_items

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,15 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-
+    if length <= 1:
+        return None
+    d = dict()
+    i = 0
+    for w in weights:
+        d[i] = w
+        for k, v in d.items():
+            if limit - w == v:
+                return (i, k)
+                break
+        i += 1
     return None
