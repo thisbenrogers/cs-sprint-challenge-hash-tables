212018ced53d1de85d94ee42c62036f21addbf35
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-09-11 21:55:06 -0500

    Implements has_negatives function

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,17 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
-
+    result = []
+    d = dict()
+    for i in a:
+        k = abs(i)
+        if k not in d:
+            d[k] = 1
+        else:
+            d[k] += 1
+    for k, v in d.items():
+        if v > 1:
+            result.append(k)
     return result
 
 
