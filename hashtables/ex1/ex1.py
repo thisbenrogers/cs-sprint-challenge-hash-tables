1468f68767f35ec26fd87e28cbcd3c8609ce484e
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-11-06 21:31:27 -0600

    Implements function

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,22 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+    # * base test
+    if length <= 1:
+        return None
+
+    # * sets up empty dictionary and index counter
+    d = dict()
+    i = 0
+
+    # * iterates through numeric values in weights list
+    for w in weights:
+        d[w] = [i, (limit - w)]     # * Makes an entry where key = weight, value = [index, pair weight]
+        i += 1                      # * Increments index
+
+    for k, v in d.items():
+        an_index = v[0]
+        pair_weight = v[1]
+        if pair_weight in d.keys() and k is not None:
+            return (d[pair_weight][0], an_index)
 
     return None
